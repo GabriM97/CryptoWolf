d0bf872af41ac1abb6550d13db46a82c653ff4e5
rscdev <rscdev777@outlook.com>
2019-02-04 19:32:35 +0100

    Added new features to cryptowolf.py, the new features are: daily candle prediction and date-time input option instead of timestamp only

--- code/Cryptowolf/cryptowolf.py
+++ code/Cryptowolf/cryptowolf.py
@@ -3,6 +3,8 @@ sys.path.append('./lib')
 from Cleaner import Cleaner
 import time
 from cryptowolf_assets import *
+from Time_Stamp_Converter import Time_Stamp_Converter
+from Extractor import Extractor
 
 pca_v=False
 
@@ -61,36 +63,125 @@ if (result==7):
 	pca_v=True
 
 
+#Here, we extract the daily opened candle, in order to predict the ClosePrice
+e = Extractor()
+daily_not_closed_candle = e.ohlc ("bitstamp","btcusd",-5,"1549152000","43200")
+
+if (type(daily_not_closed_candle)==int and x==-1): 
+	print ("Bad Request - Unable to download last candlestick from the internet\n")
+
+
+elif (type(daily_not_closed_candle)==int and x==-2):
+	print ("You have finished your CPU allowance, retry next hour - Unable to download last candlestick from the internet\n")
+
+
+elif (type(daily_not_closed_candle)==int and x==-3):
+	print ("Unexpected Error - Unable to download last candlestick from the internet\n")
+
+
+else:
+
+	pr=list()
+	pr.append( daily_not_closed_candle['result'] ['43200'] [-1] )
+	del (pr[0][4]) #We delete the temporary candle ClosePrice
+	del (pr[0][5]) #We delete the last feature because it isn't documented in the CryptoWatch API
+	print ("\n\n*******************CRYPTOWOLF-DAILY-CANDLE-PREDICTION*******************\n")
+	print ("Daily Opened Candle: \n\n {} \n".format(pr[0]))
+
+	daily_prediction=list()
+	pr=pr[0]
+
+	#If was applied normalization in the learning phase we need to apply it as well in the input query
+	if (normalization_level==1):
+		candle=normal_2_feature_scaling(pr,pack,target_features)
+
+	if (normalization_level==2):
+		candle=normal_2_mean_normalization(pr,pack,target_features)
+
+	daily_prediction.append(pr)
+		
+	#If learning was done using pca technique, we apply pca as well in the input query
+	if (pca_v==True):
+		daily_prediction=pca.transform(daily_prediction)
+
+	#Here we make the prediction
+	daily_prediction=estimator.predict(daily_prediction)
+
+	#If we perform some trasformation on the input query, we need to return back in order to show a coherent result to the user
+	if (normalization_level==1):
+		daily_prediction=feature_scaling_2_normal (daily_prediction,pack,target_features)
+
+	if (normalization_level==2):
+		daily_prediction=mean_normalization_2_normal (daily_prediction,pack,target_features)
+	
+	print ("The Predicted ClosePrice is {} \n".format(float(daily_prediction)))
+ 
+
+
 
 #Here is the core of the program, infact in this point we ask to insert a candle in order to predict her ClosePrice
 while (True):
 	
-	print ("\n\n*******************CRYPTOWOLF*******************")
+	print ("\n\n*******************CRYPTOWOLF-PREDICT-FROM-INPUT*******************\n")
 	print ("1) Predict a ClosePrice starting from a candle")
 	print ("2) Exit")
 	
 	try:
-
 		choose=int(input ("\n -> "))
-		print("")
 
+		print("")
+		
 		#Number inserted must be between 1 or 2
 		if (choose>2 or choose<1):
 			print ("Error, you have to insert a number between 1 and 2 \n")
 			continue
-
+		
 		#Here start the dialog with the user in order to make the prediction
 		if (choose==1):
 
 			candle=list()
-
+		
 			#In the fallowing code lines, we ask to the user only the features that accept the estimator			
-
+			
 			if (target_features[0]>0):
-				print ("Insert a CloseTime Value, which is a UNIX timestamp")
-				closeP=float(input ("\n -> "))
-				candle.append(closeP)
-				print("")
+				print ("Choose a CloseTime input format type, type \n 0 -> Date and Time \n 1 -> Timestamp\n")
+				choose=int(input("\n -> "))
+
+				if (choose==1):
+					print ("Insert a CloseTime Value, which must be a UNIX timestamp")
+					closeP=float(input ("\n -> "))
+
+					candle.append(closeP)
+					print("")
+				else:
+					print ("\nInsert a Day")
+					day=int(input("\n -> "))
+
+					print ("\nInsert a Month")
+					month=int(input("\n -> "))
+
+					print ("\nInsert an Year")
+					year=int(input("\n -> "))
+
+					print ("\nInsert an hour")
+					hour=int(input("\n -> "))
+
+					print ("\nInsert the minutes")
+					minutes=int(input("\n -> "))
+					print("")
+
+					risult = check_date_time (day,month,year,hour,minutes)
+					if (risult==0):
+						tp = Time_Stamp_Converter ()
+						closeT=tp.convert (day,month,year,hour,minutes)
+						closeT= float(closeT)
+						print ("You have inserted the fallowing Date and Time: {}-{}-{} {}:{} \n".format(day,month,year,hour,minutes)) 
+						print ("The corrisponding UNIX Timestamp is {} \n".format(int(closeT)))
+						candle.append(closeT)
+						print("")
+					else:
+						print ("\nError, you have inserted a wrong date and time, please retry\n")
+						continue	
 
 
 			if (target_features[1]>0):
@@ -126,7 +217,7 @@ while (True):
 
 			input_prediction=list()
 
-			
+				
 			#If was applied normalization in the learning phase we need to apply it as well in the input query
 			if (normalization_level==1):
 				candle=normal_2_feature_scaling(candle,pack,target_features)
@@ -136,7 +227,7 @@ while (True):
 
 
 			input_prediction.append(candle)
-		
+			
 			#If learning was done using pca technique, we apply pca as well in the input query
 			if (pca_v==True):
 				input_prediction=pca.transform(input_prediction)
@@ -153,15 +244,12 @@ while (True):
 
 
 			print ("\n\nThe predicted ClosePrice is {} ".format(float(prediction))) 
-		
+			
 		#Case Exit	
 		elif (choose==2):
 			break
-		
-	#Case of bad input, the program expects a numeric value		
 	except ValueError:
-		print ("Error, you have to insert a number \n")
-
+		print ("\nError, you have to insert a numeric value \n")
 
 
 #End of program
--- code/Cryptowolf/lib/cryptowolf_assets.py
+++ code/Cryptowolf/lib/cryptowolf_assets.py
@@ -1,4 +1,5 @@
 import pickle
+import calendar
 
 #This function transforms a feature scaled value into his original value
 def feature_scaling_2_normal (number,feature_scaling_data,target_features):
@@ -50,4 +51,48 @@ def check_file (filename):
 			return 7
 	except IOError:
 		return -1
+
+#This function checks the correctness of a datetime
+def check_date_time (day,month,year,hour,minutes):
+	leap=0
+
+	if (year<2):
+		return -1
+
+	if (month<1 or month>12):
+		return -2
+
+	if (hour>23 or hour<0):
+		return -3
+
+	if (minutes>59 or minutes<0):
+		return -4
+
+	if (calendar.isleap(year)):
+		leap=1
+
+	if (month==2):
+		if (leap==1):
+			if (day<1 or day>29):
+				return -5
+		else:
+			if (day<1 or day>28):
+				return -6
+
+	elif (month!=4 and month!=6 and month!=9 and month!=11):
+		if (day<1 or day>31):
+			return -7
+	else:
+		if (day<1 or day>30):
+			return -8
+	return 0
+
+def check_format (data):
+
+	if (type(data)!=int):
+		print ("Error, you have to insert a number \n")
+		return -1
+
+	return 0		
+			
 
